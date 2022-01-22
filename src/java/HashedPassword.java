a5a3a6418bae1750be706c635e659d6b18be8797
anjanarnair99 <anjanarnair99@gmail.com>
2022-01-22 21:48:59 +0530

    first

---
+++ src/java/HashedPassword.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import javax.xml.bind.DatatypeConverter;
+/**
+ *
+ * @author mahes
+ */
+public class HashedPassword {
+   
+    public String getSHA256Hash(String data) {
+        String result = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(data.getBytes("UTF-8"));
+            return bytesToHex(hash); // make it printable
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+    
+    private String  bytesToHex(byte[] hash) {
+        return DatatypeConverter.printHexBinary(hash);
+    }
+}
---
+++ src/java/NDBGeneration.java
@@ -0,0 +1,81 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mahes
+ */
+public class NDBGeneration {
+    String ndb="", temp="";
+    public String NdbGenerate(String bineryData){
+        List<String> temp2 = new ArrayList<>();
+        temp2=getParts(bineryData, 2);
+        
+        for (String s : temp2)
+        {
+            ndb += s;
+        }
+        /*getParts(bineryData, 2).stream().map((part) -> {
+            System.out.println(part);
+            System.out.println(temp);
+            return part;
+        }).map((part) -> {
+            if("00".equals(part)){
+                ndb=ndb+"0";
+                temp="0";
+               
+            }
+            return part;
+        }).map((part) -> {
+            if("01".equals(part)){
+                ndb=ndb+"1";
+                temp="1";
+            }
+            return part;
+        }).map((part) -> {
+            if("10".equals(part)){
+                ndb=ndb+"*";
+                temp="*";
+            }
+            return part;
+        }).forEachOrdered((part) -> {
+            if("11".equals(part)){
+                ndb=ndb+"*";
+                temp="*";
+            }
+            else{
+                ndb=ndb+part;
+                temp=part;
+            }
+        });*/
+        return ndb;
+    }
+    
+    private static List<String> getParts(String string, int partitionSize) {
+        List<String> parts = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+        int len = string.length();
+        for (int i=0; i<len; i+=partitionSize)
+        {
+            parts.add(string.substring(i, Math.min(len, i + partitionSize)));
+        }
+        
+        for(int j=0;j<parts.size();j++){
+            
+           if("00".equals(parts.get(j))){  temp.add("0"); }
+           else if("01".equals(parts.get(j))){ temp.add("1"); }
+           else if("10".equals(parts.get(j))){ temp.add("*"); }
+           else if("11".equals(parts.get(j))){ temp.add("*"); }
+           else{ temp.add(parts.get(j)); } 
+        }
+        return temp;
+    }
+   
+}
